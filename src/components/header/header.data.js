10e8621d0ba823b8cedcb6daec59eb6a7be0d4d3
Rashidul Hasan <crusherslab@gmail.com>
2023-05-22 02:52:42 +0600

    Project Initialize

---
+++ src/components/header/header.data.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'services',
+    label: 'Services',
+  },
+  {
+    path: 'features',
+    label: 'Feature',
+  },
+  {
+    path: 'pricing',
+    label: 'Pricing',
+  },
+  {
+    path: 'our-clients',
+    label: 'Our Clients',
+  },
+  {
+    path: 'career',
+    label: 'Career',
+  },
+
+];
